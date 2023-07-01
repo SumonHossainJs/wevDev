c50d5a2814f8533816702365e573446a03f644c6
Sumon Hossain <136088595+SumonHossainJs@users.noreply.github.com>
2023-07-01 18:54:33 +0300

    add commnet functionality

--- Server/routes/comment.route.js
+++ Server/routes/comment.route.js
@@ -1,9 +1,11 @@
-import { createComment } from "../controllers/comment.control.js";
+import { createComment ,getComment,deleteComment} from "../controllers/comment.control.js";
 import { verifyToken } from "../verifyToken.js";
 import express from 'express';
 
 const router = express.Router();
 
-router.use('/create_comment',verifyToken, createComment);
+router.post("/", verifyToken, createComment)
+router.delete("/:id", verifyToken, deleteComment)
+router.get("/:videoId", getComment)
 
 export default router;
